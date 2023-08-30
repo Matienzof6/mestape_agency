9e60e42cd1913e0edfeb600629a090ba9da85e75
Matias Pereiro <matienzof6@gmail.com>
2023-08-30 16:50:49 +0200

    configs

--- core/storage_backends.py
+++ core/storage_backends.py
@@ -1,17 +1,11 @@
-from storages.backends.s3boto3 import S3BotoStorage
+from storages.backends.s3boto3 import S3Boto3Storage
 
 
-class StaticStorage(S3BotoStorage):
+class StaticStorage(S3Boto3Storage):
     location = 'static'
     default_acl = 'private'
 
 
-class MediaStorage(S3BotoStorage):
+class MediaStore(S3Boto3Storage):
     location = 'media'
     file_overwrite = False
-
-
-
-
-StaticRootS3BotoStorage = lambda: S3BotoStorage(location='static')
-MediaRootS3BotoStorage  = lambda: S3BotoStorage(location='media')
